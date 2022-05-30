455df78409bbdce0feff9fbfe1ce15cc6983d986
YashinArt <93650761+YashinArt@users.noreply.github.com>
2022-05-30 17:17:39 +0300

    1

---
+++ app.js
@@ -0,0 +1,26 @@
+let courses = [
+    { name: "Courses in England", prices: [0, 100] }, 
+    { name: "Courses in Germany", prices: [500, null] }, 
+    { name: "Courses in Italy", prices: [100, 200] }, 
+    { name: "Courses in Russia", prices: [null, 400] },
+    { name: "Courses in China", prices: [50, 250] },
+    { name: "Courses in USA", prices: [200, null] },
+    { name: "Courses in Kazakhstan", prices: [56, 324] },
+    { name: "Courses in France", prices: [null, null] },
+]
+
+let requiredRange1 = 200;
+let requiredRange2 = [100, 350];
+let requiredRange3 = [200, null];
+
+
+
+
+
+
+
+
+
+const a = courses.flatMap(course => course.prices);
+const b = a.filter((price, index, array) => array.indexOf(price) === requiredRange1)
+console.log(b)
